24716ec140899c3b05e16782bdec7271112a012c
RanethM <raneth.20240766@iit.ac.lk>
2026-04-17 12:14:09 +0530

    Made the GET ("/rooms") endpoint where it will get all the rooms in the campus

--- src/main/java/com/mycompany/smartcampusapi/resources/RoomResource.java
+++ src/main/java/com/mycompany/smartcampusapi/resources/RoomResource.java
@@ -4,10 +4,28 @@
  */
 package com.mycompany.smartcampusapi.resources;
 
+import com.mycompany.smartcampusapi.database.DataStore;
+import com.mycompany.smartcampusapi.model.Room;
+import java.util.Collection;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
 /**
  *
  * @author ranet
+ *
  */
+@Path("/rooms")
 public class RoomResource {
     
+    
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Room> getAllRooms(){ 
+        return DataStore.rooms.values();
+    }
 }

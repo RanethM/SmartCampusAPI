9937f8dda6ac8351a444cd4953a281b6500e02b6
RanethM <raneth.20240766@iit.ac.lk>
2026-04-17 16:45:58 +0530

    Edited the sensor resource to add a sub resource locator of "/{sensorId}/readings"

--- src/main/java/com/mycompany/smartcampusapi/resources/SensorResource.java
+++ src/main/java/com/mycompany/smartcampusapi/resources/SensorResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -70,6 +71,11 @@ public class SensorResource {
             .entity(sensor)
             .build(); 
     }
+    
+    @Path("/{sensorId}/readings")
+    public SensorReadingResource getReadingResource(@PathParam("sensorId") String sensorId) {
+    return new SensorReadingResource(sensorId);
+
     
     
 }

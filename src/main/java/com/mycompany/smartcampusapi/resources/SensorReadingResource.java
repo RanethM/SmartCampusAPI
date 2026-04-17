3858fc6d5f3350bafbabd62b6b615228382fcc18
RanethM <raneth.20240766@iit.ac.lk>
2026-04-17 16:52:09 +0530

    Made a new sensor reading resource and added a constructor with the sensorId

---
+++ src/main/java/com/mycompany/smartcampusapi/resources/SensorReadingResource.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampusapi.resources;
+
+import com.mycompany.smartcampusapi.database.DataStore;
+import com.mycompany.smartcampusapi.model.Sensor;
+import com.mycompany.smartcampusapi.model.SensorReading;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ranet
+ */
+public class SensorReadingResource {
+    private String sensorId; 
+    
+    public SensorReadingResource(String sensorId){
+        this.sensorId = sensorId;
+        
+    }
+    
+    
+    
+}

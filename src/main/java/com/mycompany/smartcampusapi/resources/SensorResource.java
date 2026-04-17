476495d2d4c230afe47db751ccd2f506f546c3e6
RanethM <raneth.20240766@iit.ac.lk>
2026-04-17 16:22:15 +0530

    Created a POST ("/sensor") to add data of the sensor and linked the sensor to the room

--- src/main/java/com/mycompany/smartcampusapi/resources/SensorResource.java
+++ src/main/java/com/mycompany/smartcampusapi/resources/SensorResource.java
@@ -5,14 +5,18 @@
 package com.mycompany.smartcampusapi.resources;
 
 import com.mycompany.smartcampusapi.database.DataStore;
+import com.mycompany.smartcampusapi.model.Room;
 import com.mycompany.smartcampusapi.model.Sensor;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -31,9 +35,41 @@ public class SensorResource {
                     
         }
         
+        //Allows to filter sensors using the query params
+        
         return DataStore.sensors.values().stream() 
                 .filter(sensor -> sensor.getType().equalsIgnoreCase(type))
                 .collect(Collectors.toList()); 
     }
+    
+    
+    //POST a new sensor
+    @POST 
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    
+    public Response addSensor(Sensor sensor){
+    //First we validate 
+    Room room = DataStore.rooms.get(sensor.getRoomId()); 
+    
+    if (room== null){ 
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity("Room does not exist")
+                .build(); 
+    }
+    
+    // Save the sensor in the DataStore 
+    
+    DataStore.sensors.put(sensor.getId(),sensor); 
+    
+    //Link the sensors to the room 
+    
+    room.getSensorIds().add(sensor.getId()); 
+    
+    return Response.status(Response.Status.CREATED)
+            .entity(sensor)
+            .build(); 
+    }
+    
     
 }

a0be64aa8bdb77b942107a1c290e23d65b645b5a
RanethM <raneth.20240766@iit.ac.lk>
2026-04-17 12:09:02 +0530

    Made a new class called DataStore which acts as our database in the coursework which stores data.

---
+++ src/main/java/com/mycompany/smartcampusapi/database/DataStore.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smartcampusapi.database;
+
+import com.mycompany.smartcampusapi.model.Room;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author ranet
+ */
+public class DataStore {
+    
+    public static Map<String, Room> rooms = new HashMap<>();
+
+    static {
+        rooms.put("R1", new Room("R1", "Library", 50));
+        rooms.put("R2", new Room("R2", "Lab", 30));
+    }
+    
+}

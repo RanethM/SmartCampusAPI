ce16148a597215fdb8435d3c0bfcfffc6319943b
RanethM <raneth.20240766@iit.ac.lk>
2026-04-21 11:53:14 +0530

    made a mapper for the linkedresourcenotfound exception

---
+++ src/main/java/com/mycompany/smartcampusapi/exception/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,25 @@
+package com.mycompany.smartcampusapi.exception;
+
+import com.mycompany.smartcampusapi.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException exception) {
+
+        ErrorMessage error = new ErrorMessage(
+                exception.getMessage(),
+                422
+        );
+
+        return Response.status(422)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
